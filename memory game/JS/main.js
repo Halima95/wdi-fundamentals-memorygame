1cc741cca64d9d7988bc4b8e78c1d63a016b322e
Halima <halima@Halimas-MacBook-Air.local>
2018-09-26 16:31:44 +0300

    created game logic

---
+++ memory game/JS/main.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+var cards = [ "Queen", "Queen", "King","King" ];
+
+var cardOne = cards[0];
+
+var cardsInPlay= [];
+
+var checkForMatch = function(){
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} 
+else {
+alert("Sorry, try again.");
+}
+
+}
+
+
+var flipCard = function(cardId) {
+
+	
+	console.log("User flipped" + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+};
+
+flipCard(0);
+ 	
+flipCard(2);
+checkForMatch();
+
+
+
+
+
+
